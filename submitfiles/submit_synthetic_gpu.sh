81fa967e918f6af866e6bc467ae935c0705afe10
ansol <ansol@dtu.dk>
2023-01-23 18:25:50 +0100

    changed submit files

--- submitfiles/submit_synthetic_gpu.sh
+++ submitfiles/submit_synthetic_gpu.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-#BSUB -J syntheticjob
-#BSUB -q hpc
-#BSUB -R "rusage[mem=8GB]"
+#BSUB -J syntheticgpujob
+#BSUB -q gpua100
+#BSUB -R "rusage[mem=30GB]"
 #BSUB -B
 #BSUB -N
-#BSUB -o syntheticjob_out_%J.txt
-#BSUB -e syntheticjob_err_%J.txt
+#BSUB -o syntheticgpujob_out_%J.txt
+#BSUB -e syntheticgpujob_err_%J.txt
 #BSUB -W 10:00 
-#BSUB -n 16
-#BSUB -R "span[hosts=1]"
+#BSUB -n 1
+#BSUB -gpu "num=1:mode=exclusive_process"
 
 # -- commands you want to execute -- 
 module load python3/3.10.7
