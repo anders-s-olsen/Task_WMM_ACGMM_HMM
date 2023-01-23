8ac5b49c7128a9e3ce631d9ba7d8878fe932fbad
ansol <ansol@dtu.dk>
2023-01-23 15:31:28 +0100

    visualizations

--- submitfiles/submit_synthetic.sh
+++ submitfiles/submit_synthetic.sh
@@ -12,11 +12,10 @@
 
 # -- commands you want to execute -- 
 module load python3/3.10.7
-module load numpy
-module load matplotlib
-module load h5py
-python -m pip3 install --user tqdm torch
+module load numpy/1.23.3-python-3.10.7-openblas-0.3.21
+module load matplotlib/3.6.0-numpy-1.23.3-python-3.10.7
+module load h5py/3.7.0-python-3.10.7
+python3 -m pip install --user tqdm torch
 cd ..
 cd notebooks
-python Synthetic.py
-matlab -nodisplay -batch analyze_k_results
+python3 Synthetic_LR.py
