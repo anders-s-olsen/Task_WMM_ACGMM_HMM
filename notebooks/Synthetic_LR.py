74589d64b8648953b30bf2fb817237600f63eaa9
Anders <ansol@dtu.dk>
2023-01-24 19:12:24 +0100

    Plot fix

--- notebooks/Synthetic_LR.py
+++ notebooks/Synthetic_LR.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[4]:
 
 
 ## Imports and get_param
@@ -13,7 +13,7 @@ import h5py
 import torch
 from torch import optim
 import numpy as np
-from tqdm import tqdm
+from tqdm.notebook import trange, tqdm
 import matplotlib.pyplot as plt
 
 sys.path.append(os.path.abspath(os.path.join('..')))
@@ -38,7 +38,7 @@ def get_param(model, show=True):
 
 # ## Load data and get correct shape and dtype
 
-# In[2]:
+# In[5]:
 
 
 synth_dataset = '../data/synthetic/HMMdata.h5'
@@ -50,10 +50,10 @@ print(data.dtype)
 print(torch.norm(data,dim=2))
 
 
-# ## ACG mixture
+# ## mixtures with learned LRs
 # train model with diff learning rates, get best model 
 
-# In[12]:
+# In[6]:
 
 
 eval_LR = np.arange(0.1, 1, 0.1)
@@ -65,18 +65,18 @@ watson_hmm_LR_results = np.zeros((len(eval_LR),int_epoch))
 
 for idx, LR in enumerate(tqdm(eval_LR)):
     #
-    ACG_MM = TorchMixtureModel(distribution_object=ACG,K=2, dist_dim=3)
-    ACG_HMM = HMM(num_states=2, observation_dim=3, emission_dist=ACG)
+    #ACG_MM = TorchMixtureModel(distribution_object=ACG,K=2, dist_dim=3)
+    #ACG_HMM = HMM(num_states=2, observation_dim=3, emission_dist=ACG)
     Watson_MM = TorchMixtureModel(distribution_object=Watson,K=2, dist_dim=3)
     Watson_HMM = HMM(num_states=2, observation_dim=3, emission_dist=Watson)
     
-    ACG_MM_optimizer = optim.Adam(ACG_MM.parameters(), lr=LR)
-    ACG_MM_ll = train_hmm(ACG_MM, data=torch.squeeze(data), optimizer=ACG_MM_optimizer, num_epoch=int_epoch, keep_bar=False)
-    acg_mm_LR_results[idx] = ACG_MM_ll
+    #ACG_MM_optimizer = optim.Adam(ACG_MM.parameters(), lr=LR)
+    #ACG_MM_ll = train_hmm(ACG_MM, data=torch.squeeze(data), optimizer=ACG_MM_optimizer, num_epoch=int_epoch, keep_bar=False)
+    #acg_mm_LR_results[idx] = ACG_MM_ll
     
-    ACG_HMM_optimizer = optim.Adam(ACG_HMM.parameters(), lr=LR)
-    ACG_HMM_ll = train_hmm(ACG_HMM, data=data, optimizer=ACG_HMM_optimizer, num_epoch=int_epoch, keep_bar=False)
-    acg_hmm_LR_results[idx] = ACG_HMM_ll
+    #ACG_HMM_optimizer = optim.Adam(ACG_HMM.parameters(), lr=LR)
+    #ACG_HMM_ll = train_hmm(ACG_HMM, data=data, optimizer=ACG_HMM_optimizer, num_epoch=int_epoch, keep_bar=False)
+    #acg_hmm_LR_results[idx] = ACG_HMM_ll
     
     Watson_MM_optimizer = optim.Adam(Watson_MM.parameters(), lr=LR)
     Watson_MM_ll = train_hmm(Watson_MM, data=torch.squeeze(data), optimizer=Watson_MM_optimizer, num_epoch=int_epoch, keep_bar=False)
@@ -90,37 +90,38 @@ for idx, LR in enumerate(tqdm(eval_LR)):
 
 # ## plot of learning rates
 
-# In[7]:
+# In[ ]:
 
 
 plt.close()
+get_ipython().run_line_magic('matplotlib', 'inline')
 fig, axs = plt.subplots(2, 2,figsize=(15, 15))
 
-for ax in axs:
-    axs[ax].xlabel('Epochs')
-    axs[ax].ylabel('Log-Likelihood')
-    axs[ax].legend(np.round(eval_LR, 3), ncol=1, bbox_to_anchor=(1.01, 1.05), loc='upper left', borderaxespad=0.)
-    axs[ax].grid()
+#for ax in 1:len(axs):
+#    axs[ax].xlabel('Epochs')
+#    axs[ax].ylabel('Log-Likelihood')
+#    axs[ax].legend(np.round(eval_LR, 3), ncol=1, bbox_to_anchor=(1.01, 1.05), loc='upper left', borderaxespad=0.)
+#    axs[ax].grid()
 
 
 axs[0].plot(acg_mm_LR_results.T)
 axs[0].title('Learning rate - ACG - Synthetic3D - mixture model')
 
-axs[0].plot(acg_hmm_LR_results.T)
-axs[0].title('Learning rate - ACG - Synthetic3D - Hidden Markov Model')
+axs[1].plot(acg_hmm_LR_results.T)
+axs[1].title('Learning rate - ACG - Synthetic3D - Hidden Markov Model')
 
-axs[0].plot(watson_mm_LR_results.T)
-axs[0].title('Learning rate - Watson - Synthetic3D - mixture model')
+axs[2].plot(watson_mm_LR_results.T)
+axs[2].title('Learning rate - Watson - Synthetic3D - mixture model')
 
-axs[0].plot(watson_hmm_LR_results.T)
-axs[0].title('Learning rate - Watson - Synthetic3D - Hidden Markov Model')
+axs[3].plot(watson_hmm_LR_results.T)
+axs[3].title('Learning rate - Watson - Synthetic3D - Hidden Markov Model')
 
 plt.savefig('../reports/synthetic_methods/synthetic_LRs.png')
 
 
 # ## get best models
 
-# In[11]:
+# In[ ]:
 
 
 #acg_mm_idx = np.argmax(acg_mm_LR_results)
@@ -141,7 +142,7 @@ plt.savefig('../reports/synthetic_methods/synthetic_LRs.png')
 
 # ## extract parameters from the best model (should be pi=0.5)
 
-# In[12]:
+# In[ ]:
 
 
 #acgbest_param = get_param(best_model)
