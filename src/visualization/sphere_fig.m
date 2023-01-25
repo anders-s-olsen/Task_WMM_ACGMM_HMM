f046b417a124af6239c07fb3fe781806626b903a
ansol <ansol@dtu.dk>
2023-01-25 09:06:42 +0100

    various data

--- src/visualization/sphere_fig.m
+++ src/visualization/sphere_fig.m
@@ -28,118 +28,59 @@ rng default
 delete('data/synthetic/HMMdata.h5')
 h5create('data/synthetic/HMMdata.h5','/X',size(X))
 h5write('data/synthetic/HMMdata.h5','/X',X)
-%% Figure random data
 
-gridPoints = 1000;
-[x,y,z] = sphere(gridPoints);
-figure('units','normalized','outerposition',[0 0 .5 1]); clf;%'visible','off',
-
-sh(1) = surf(x,y,z, 'FaceAlpha', .2, 'EdgeAlpha', .1,'EdgeColor','none','FaceColor','none');
-hold on; axis equal;
-xlabel('x'); ylabel('y'); zlabel('z');
-
-% smaller sphere to show lines on
-[x2,y2,z2] = sphere(20); %30
-sh(2) = surf(x2,y2,z2, 'EdgeAlpha', .2,'FaceColor','none','EdgeColor',[0,0,0]);
-
-%
-% figure;
-cols = [0,0.4,0;0.5,0,0;0,0,0.5];
-set(gca,'XColor', 'none','YColor','none','ZColor','none')
-grid off
-
-for i = 1:size(SIGMAs,3)
-scatter3(X(cluster_id==i,1), X(cluster_id==i,2), X(cluster_id==i,3),7,cols(i,:),'filled');
-end
-
+%% Figure 1
+pointsspherefig(X,cluster_id);
 view(-29,-13)
-exportgraphics(gcf,[ff,'sphere_WMM_data.png'],'Resolution',300)
-%% Figure fit, Sphere, contour
+% exportgraphics(gcf,[ff,'sphere_WMM_data.png'],'Resolution',300)
 
-gridPoints = 1000;
-[XX,YY,ZZ] = sphere(gridPoints);
-figure('units','normalized','outerposition',[0.5 0 .5 1]); clf;%'visible','off',
-ax1 = axes;
-sh(1) = surf(XX,YY,ZZ, 'FaceAlpha', .2, 'EdgeAlpha', .1,'EdgeColor','none','FaceColor','none');
-hold on; axis equal;
-xlabel('x'); ylabel('y'); zlabel('z');
-
-% smaller sphere to show lines on
-[x2,y2,z2] = sphere(20); %30
-sh(2) = surf(x2,y2,z2, 'EdgeAlpha', .2,'FaceColor','none','EdgeColor',[0,0,0]);
-set(gca,'XColor', 'none','YColor','none','ZColor','none')
-grid off
-
-addpath('/dtu-compute/HCP_dFC/2023/hcp_dfc/src/models/')
-WMM_results = WMM_EM_BigMem2(X,2,200,1,'++',0)
-
-mu1 = WMM_results.mu(:,1);mu2 = WMM_results.mu(:,2);
-% m = [m1;m2];
-[~,mini] = min(WMM_results.kappa)
-multi = [1,1];multi(mini)=8;
-kappa = multi'.*WMM_results.kappa;
-
-T1 = nan(size(XX));T2 = nan(size(XX));
-
-varfactor = 4;
-
-for i = 1:size(XX,1)
-    for j = 1:size(XX,2)
-        tmp = [XX(i,j),YY(i,j),ZZ(i,j)];
-        if (tmp*mu1).^2>1-varfactor./kappa(1)
-            T1(i,j) = (tmp*mu1).^2;
-        elseif (tmp*mu2).^2>1-varfactor./kappa(2)
-            T2(i,j) = (tmp*mu2).^2;
-        end
-    end
-end
-
-% d1 = m1./norm(m1);d2 = m2./norm(m2);d3 = m3./norm(m3);
-% line([0,d1(1)],[0,d1(2)],[0,d1(3)],'Color',col1,'LineWidth',2.5)
-% line([0,d2(1)],[0,d2(2)],[0,d2(3)],'Color',col2,'LineWidth',2.5)
-% line([0,d3(1)],[0,d3(2)],[0,d3(3)],'Color',col3,'LineWidth',2.5)
-
-ax2 = axes;
-sh(3) = surf(ax2,XX,YY,ZZ);
-set(sh(3),'EdgeColor','none');
-set(sh(3),'CData',(T1-min(T1(:)))./(max(T1(:))-min(T1(:))));
-
-ax3 = axes;
-sh(4) = surf(ax3,XX,YY,ZZ);
-set(sh(4),'EdgeColor','none');
-set(sh(4),'CData',(T2-min(T2(:)))./(max(T2(:))-min(T2(:))));
-
-% ax4 = axes;
-% sh(4) = surf(ax4,XX,YY,ZZ);
-% set(sh(4),'EdgeColor','none');
-% set(sh(4),'CData',(T3-min(T3(:)))./(max(T3(:))-min(T3(:))));
-
-
-hlink = linkprop([ax1,ax2,ax3],{'XLim','YLim','ZLim','CameraUpVector','CameraPosition','CameraTarget','CameraViewAngle'});
-% linkaxes([ax1,ax2,ax3,ax4])
-ax2.Visible = 'off';
-ax2.XTick = [];
-ax2.YTick = [];
-ax3.Visible = 'off';
-ax3.XTick = [];
-ax3.YTick = [];
-% ax4.Visible = 'off';
-% ax4.XTick = [];
-% ax4.YTick = [];
-
-col1 = [0,0.4,0];
-col2 = [0.5,0,0];
-% col3 = [0.5,0,0.5];
-
-cmaps{1} = ([linspace(1,0,256)',linspace(1,0.5,256)',linspace(1,0,256)']);
-cmaps{2} = ([linspace(1,0.5,256)',linspace(1,0,256)',linspace(1,0,256)']);
-
-% 
-colormap(ax2,cmaps{1})
-colormap(ax3,cmaps{2})
+%% Figure 2
+%%% ACG MM
+A1 = table2array(readtable('data/synthetic/ACG_MM_comp0.csv'))/1000;
+A2 = table2array(readtable('data/synthetic/ACG_MM_comp1.csv'))/1000;
+contourspherefig([],[],cat(3,A1,A2))
+title('Watson mixture')
 view(-29,-13)
-exportgraphics(gcf,[ff,'sphere_WMM_contour.png'],'Resolution',300)
-close all
+% exportgraphics(gcf,[ff,'sphere_WMM_contour.png'],'Resolution',300)
+
+%%% Watson MM
+mu1 = table2array(readtable('data/synthetic/Watson_MM_comp0mu.csv'));
+kappa1 = table2array(readtable('data/synthetic/Watson_MM_comp0kappa.csv'));
+mu2 = table2array(readtable('data/synthetic/Watson_MM_comp1mu.csv'));
+kappa2 = table2array(readtable('data/synthetic/Watson_MM_comp1kappa.csv'));
+contourspherefig([mu1,mu2],[kappa2,kappa2],[])
+title('Watson mixture')
+view(-29,-13)
+% exportgraphics(gcf,[ff,'sphere_WMM_contour.png'],'Resolution',300)
+
+%%% Watson MM
+mu1 = table2array(readtable('data/synthetic/Watson_MM_comp0mu.csv'));
+kappa1 = table2array(readtable('data/synthetic/Watson_MM_comp0kappa.csv'));
+mu2 = table2array(readtable('data/synthetic/Watson_MM_comp1mu.csv'));
+kappa2 = table2array(readtable('data/synthetic/Watson_MM_comp1kappa.csv'));
+contourspherefig([mu1,mu2],[kappa2,kappa2],[])
+title('Watson mixture')
+view(-29,-13)
+% exportgraphics(gcf,[ff,'sphere_WMM_contour.png'],'Resolution',300)
+
+%%% Watson MM
+mu1 = table2array(readtable('data/synthetic/Watson_MM_comp0mu.csv'));
+kappa1 = table2array(readtable('data/synthetic/Watson_MM_comp0kappa.csv'));
+mu2 = table2array(readtable('data/synthetic/Watson_MM_comp1mu.csv'));
+kappa2 = table2array(readtable('data/synthetic/Watson_MM_comp1kappa.csv'));
+contourspherefig([mu1,mu2],[kappa2,kappa2],[])
+title('Watson mixture')
+view(-29,-13)
+% exportgraphics(gcf,[ff,'sphere_WMM_contour.png'],'Resolution',300)
+
+
+
+
+
+
+
+
+
 %% function
 function [X,cluster_allocation] = syntheticMixture3D(PI,SIGMAs,num_points)
 
@@ -194,11 +135,134 @@ end
 X_emission = squeeze(X_emission);
 end
 
+%% Figure with random data
+
+function pointsspherefig(X,cluster_id)
+gridPoints = 1000;
+[x,y,z] = sphere(gridPoints);
+figure('units','normalized','outerposition',[0 0 .5 1]); clf;%'visible','off',
+
+sh(1) = surf(x,y,z, 'FaceAlpha', .2, 'EdgeAlpha', .1,'EdgeColor','none','FaceColor','none');
+hold on; axis equal;
+xlabel('x'); ylabel('y'); zlabel('z');
+
+% smaller sphere to show lines on
+[x2,y2,z2] = sphere(20); %30
+sh(2) = surf(x2,y2,z2, 'EdgeAlpha', .2,'FaceColor','none','EdgeColor',[0,0,0]);
+
+%
+% figure;
+cols = [0,0.4,0;0.5,0,0;0,0,0.5];
+set(gca,'XColor', 'none','YColor','none','ZColor','none')
+grid off
+
+for i = 1:numel(unique(cluster_id))
+scatter3(X(cluster_id==i,1), X(cluster_id==i,2), X(cluster_id==i,3),7,cols(i,:),'filled');
+end
+end
+
+%% Figure fit, Sphere, contour
+function contourspherefig(mu,kappa,A)
+
+gridPoints = 1000;
+[XX,YY,ZZ] = sphere(gridPoints);
+figure('units','normalized','outerposition',[0.5 0 .5 1]); clf;%'visible','off',
+ax1 = axes;
+sh(1) = surf(XX,YY,ZZ, 'FaceAlpha', .2, 'EdgeAlpha', .1,'EdgeColor','none','FaceColor','none');
+hold on; axis equal;
+xlabel('x'); ylabel('y'); zlabel('z');
+
+% smaller sphere to show lines on
+[x2,y2,z2] = sphere(20); %30
+sh(2) = surf(x2,y2,z2, 'EdgeAlpha', .2,'FaceColor','none','EdgeColor',[0,0,0]);
+set(gca,'XColor', 'none','YColor','none','ZColor','none')
+grid off
+
+addpath('/dtu-compute/HCP_dFC/2023/hcp_dfc/src/models/')
+% WMM_results = WMM_EM_BigMem2(X,2,200,1,'++',0)
+% mu1 = WMM_results.mu(:,1);mu2 = WMM_results.mu(:,2);
+% kappa = WMM_results.kappa;
+
+% m = [m1;m2];
+% [~,mini] = min(kappa)
+% multi = [1,1];multi(mini)=8;
+% kappa = multi'.*kappa;
+
+T1 = nan(size(XX));T2 = nan(size(XX));
+
+varfactor = 0.4;
+
+if ~isempty(mu)&&~isempty(kappa)
+    kappa = kappa*10;
+    for i = 1:size(XX,1)
+        for j = 1:size(XX,2)
+            tmp = [XX(i,j),YY(i,j),ZZ(i,j)];
+            if (tmp*mu(:,1)).^2>1-varfactor./kappa(1)
+                T1(i,j) = (tmp*mu(:,1)).^2;
+            elseif (tmp*mu(:,2)).^2>1-varfactor./kappa(2)
+                T2(i,j) = (tmp*mu(:,2)).^2;
+            end
+        end
+    end
+elseif ~isempty(A)
+    L_chol1 = chol(A(:,:,1),'lower');
+    L_chol2 = chol(A(:,:,2),'lower');
+    for i = 1:size(XX,1)
+        for j = 1:size(XX,2)
+            tmp = [XX(i,j),YY(i,j),ZZ(i,j)];
+            if norm(L_chol1*tmp').^2>1-varfactor
+                T1(i,j) = norm((L_chol1*tmp')).^2;
+            elseif norm(L_chol2*tmp').^2>1-varfactor
+                T2(i,j) = norm((L_chol2*tmp')).^2;
+            end
+        end
+    end
+end
+
+% d1 = m1./norm(m1);d2 = m2./norm(m2);d3 = m3./norm(m3);
+% line([0,d1(1)],[0,d1(2)],[0,d1(3)],'Color',col1,'LineWidth',2.5)
+% line([0,d2(1)],[0,d2(2)],[0,d2(3)],'Color',col2,'LineWidth',2.5)
+% line([0,d3(1)],[0,d3(2)],[0,d3(3)],'Color',col3,'LineWidth',2.5)
+
+ax2 = axes;
+sh(3) = surf(ax2,XX,YY,ZZ);
+set(sh(3),'EdgeColor','none');
+set(sh(3),'CData',(T1-min(T1(:)))./(max(T1(:))-min(T1(:))));
+
+ax3 = axes;
+sh(4) = surf(ax3,XX,YY,ZZ);
+set(sh(4),'EdgeColor','none');
+set(sh(4),'CData',(T2-min(T2(:)))./(max(T2(:))-min(T2(:))));
+
+% ax4 = axes;
+% sh(4) = surf(ax4,XX,YY,ZZ);
+% set(sh(4),'EdgeColor','none');
+% set(sh(4),'CData',(T3-min(T3(:)))./(max(T3(:))-min(T3(:))));
 
 
+hlink = linkprop([ax1,ax2,ax3],{'XLim','YLim','ZLim','CameraUpVector','CameraPosition','CameraTarget','CameraViewAngle'});
+% linkaxes([ax1,ax2,ax3,ax4])
+ax2.Visible = 'off';
+ax2.XTick = [];
+ax2.YTick = [];
+ax3.Visible = 'off';
+ax3.XTick = [];
+ax3.YTick = [];
+% ax4.Visible = 'off';
+% ax4.XTick = [];
+% ax4.YTick = [];
 
+col1 = [0,0.4,0];
+col2 = [0.5,0,0];
+% col3 = [0.5,0,0.5];
 
+cmaps{1} = ([linspace(1,0,256)',linspace(1,0.5,256)',linspace(1,0,256)']);
+cmaps{2} = ([linspace(1,0.5,256)',linspace(1,0,256)',linspace(1,0,256)']);
 
+% 
+colormap(ax2,cmaps{1})
+colormap(ax3,cmaps{2})
+end
 
 
 
