eaa1c0dc3e4b4e41f451c5099d7202dadb4c634c
ansol <ansol@dtu.dk>
2023-01-25 11:40:35 +0100

    changed synth data

--- src/visualization/sphere_fig.m
+++ src/visualization/sphere_fig.m
@@ -24,60 +24,57 @@ T(2,2) = mean(next2==2);
 % [X, cluster_id] = syntheticMixture3D(PI, SIGMAs, size(PI,1));
 rng default
 % [X,cluster_id] = syntheticHMM(PI,SIGMAs, T,size(PI,1),1);
+if ~exist('data/synthetic/HMMdata.h5')
 [X,cluster_id] = syntheticMixture3D(PI,SIGMAs,size(PI,1));
-delete('data/synthetic/HMMdata.h5')
 h5create('data/synthetic/HMMdata.h5','/X',size(X))
 h5write('data/synthetic/HMMdata.h5','/X',X)
-
+h5create('data/synthetic/HMMdata.h5','/cluster_id',size(cluster_id))
+h5write('data/synthetic/HMMdata.h5','/cluster_id',cluster_id)
+else
+    X = h5read('data/synthetic/HMMdata.h5','/X');
+    cluster_id = h5read('data/synthetic/HMMdata.h5','/cluster_id');
+end
 %% Figure 1
 pointsspherefig(X,cluster_id);
 view(-29,-13)
 % exportgraphics(gcf,[ff,'sphere_WMM_data.png'],'Resolution',300)
 
 %% Figure 2
+%%% Watson MM
+mu1 = table2array(readtable('data/synthetic/Watson_MM_comp0mu.csv'));
+kappa1 = table2array(readtable('data/synthetic/Watson_MM_comp0kappa.csv'));
+mu2 = table2array(readtable('data/synthetic/Watson_MM_comp1mu.csv'));
+kappa2 = table2array(readtable('data/synthetic/Watson_MM_comp1kappa.csv'));
+contourspherefig([mu1,mu2],[kappa2,kappa2],[])
+title('Watson mixture')
+view(-29,-13)
+% exportgraphics(gcf,[ff,'sphere_WMM_contour.png'],'Resolution',300)
+
 %%% ACG MM
-A1 = table2array(readtable('data/synthetic/ACG_MM_comp0.csv'))/1000;
-A2 = table2array(readtable('data/synthetic/ACG_MM_comp1.csv'))/1000;
+A1 = table2array(readtable('data/synthetic/ACG_MM_comp0.csv'));
+A2 = table2array(readtable('data/synthetic/ACG_MM_comp1.csv'));
 contourspherefig([],[],cat(3,A1,A2))
-title('Watson mixture')
+gcf,title('ACG mixture')
 view(-29,-13)
 % exportgraphics(gcf,[ff,'sphere_WMM_contour.png'],'Resolution',300)
 
-%%% Watson MM
-mu1 = table2array(readtable('data/synthetic/Watson_MM_comp0mu.csv'));
-kappa1 = table2array(readtable('data/synthetic/Watson_MM_comp0kappa.csv'));
-mu2 = table2array(readtable('data/synthetic/Watson_MM_comp1mu.csv'));
-kappa2 = table2array(readtable('data/synthetic/Watson_MM_comp1kappa.csv'));
+%%% Watson HMM
+mu1 = table2array(readtable('data/synthetic/Watson_HMM_comp0mu.csv'));
+kappa1 = table2array(readtable('data/synthetic/Watson_HMM_comp0kappa.csv'));
+mu2 = table2array(readtable('data/synthetic/Watson_HMM_comp1mu.csv'));
+kappa2 = table2array(readtable('data/synthetic/Watson_HMM_comp1kappa.csv'));
 contourspherefig([mu1,mu2],[kappa2,kappa2],[])
-title('Watson mixture')
+title('Watson HMM')
 view(-29,-13)
 % exportgraphics(gcf,[ff,'sphere_WMM_contour.png'],'Resolution',300)
 
-%%% Watson MM
-mu1 = table2array(readtable('data/synthetic/Watson_MM_comp0mu.csv'));
-kappa1 = table2array(readtable('data/synthetic/Watson_MM_comp0kappa.csv'));
-mu2 = table2array(readtable('data/synthetic/Watson_MM_comp1mu.csv'));
-kappa2 = table2array(readtable('data/synthetic/Watson_MM_comp1kappa.csv'));
-contourspherefig([mu1,mu2],[kappa2,kappa2],[])
-title('Watson mixture')
+%%% ACG HMM
+A1 = table2array(readtable('data/synthetic/ACG_HMM_comp0.csv'));
+A2 = table2array(readtable('data/synthetic/ACG_HMM_comp1.csv'));
+contourspherefig([],[],cat(3,A1,A2))
+gcf,title('ACG HMM')
 view(-29,-13)
 % exportgraphics(gcf,[ff,'sphere_WMM_contour.png'],'Resolution',300)
-
-%%% Watson MM
-mu1 = table2array(readtable('data/synthetic/Watson_MM_comp0mu.csv'));
-kappa1 = table2array(readtable('data/synthetic/Watson_MM_comp0kappa.csv'));
-mu2 = table2array(readtable('data/synthetic/Watson_MM_comp1mu.csv'));
-kappa2 = table2array(readtable('data/synthetic/Watson_MM_comp1kappa.csv'));
-contourspherefig([mu1,mu2],[kappa2,kappa2],[])
-title('Watson mixture')
-view(-29,-13)
-% exportgraphics(gcf,[ff,'sphere_WMM_contour.png'],'Resolution',300)
-
-
-
-
-
-
 
 
 
@@ -193,7 +190,7 @@ T1 = nan(size(XX));T2 = nan(size(XX));
 varfactor = 0.4;
 
 if ~isempty(mu)&&~isempty(kappa)
-    kappa = kappa*10;
+%     kappa = kappa*10;
     for i = 1:size(XX,1)
         for j = 1:size(XX,2)
             tmp = [XX(i,j),YY(i,j),ZZ(i,j)];
@@ -205,16 +202,34 @@ if ~isempty(mu)&&~isempty(kappa)
         end
     end
 elseif ~isempty(A)
-    L_chol1 = chol(A(:,:,1),'lower');
-    L_chol2 = chol(A(:,:,2),'lower');
+%     L_chol1 = chol(A(:,:,1),'lower');
+%     L_chol2 = chol(A(:,:,2),'lower');
+likelihood_threshold = -7;
+Cp(1) = gammaln(1.5)-1.5*log(2*pi)+log(inv(sqrt(det(A(:,:,1)))));
+Cp(2) = gammaln(1.5)-1.5*log(2*pi)+log(inv(sqrt(det(A(:,:,1)))));
+
+A1_inv = inv(A(:,:,1));
+A2_inv = inv(A(:,:,2));
+
     for i = 1:size(XX,1)
         for j = 1:size(XX,2)
             tmp = [XX(i,j),YY(i,j),ZZ(i,j)];
-            if norm(L_chol1*tmp').^2>1-varfactor
-                T1(i,j) = norm((L_chol1*tmp')).^2;
-            elseif norm(L_chol2*tmp').^2>1-varfactor
-                T2(i,j) = norm((L_chol2*tmp')).^2;
+%             ll1(i,j) = Cp(1)+(-1.5)*log(tmp*A1_inv*tmp');
+%             ll2(i,j) = Cp(2)+(-1.5)*log(tmp*A2_inv*tmp');
+            
+            
+            if Cp(1)+(-1.5)*log(tmp*A1_inv*tmp')>likelihood_threshold
+                T1(i,j) = Cp(1)+(-1.5)*log(tmp*A1_inv*tmp');
+            elseif Cp(2)+(-1.5)*log(tmp*A2_inv*tmp')>likelihood_threshold
+                T2(i,j) = Cp(2)+(-1.5)*log(tmp*A2_inv*tmp');
             end
+            
+            
+%             if norm(L_chol1*tmp').^2>1-varfactor
+%                 T1(i,j) = norm((L_chol1*tmp')).^2;
+%             elseif norm(L_chol2*tmp').^2>1-varfactor
+%                 T2(i,j) = norm((L_chol2*tmp')).^2;
+%             end
         end
     end
 end
