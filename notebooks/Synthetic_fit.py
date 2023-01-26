d0d78d5303dee5bf28b1bec9dea8fe537c41cd31
Anders <ansol@dtu.dk>
2023-01-26 10:31:33 +0100

    fewer epochs

--- notebooks/Synthetic_fit.py
+++ notebooks/Synthetic_fit.py
@@ -57,7 +57,7 @@ data = torch.unsqueeze(torch.transpose(data,dim0=0,dim1=1),dim=0).float()
 
 num_reps = 2
 best_LR = 0.01
-int_epoch = 1000
+int_epoch = 100
 
 for m in range(4):
     ll = np.zeros((num_reps,int_epoch))
