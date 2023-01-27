040d71b7a8ade2b909c44f689ce4446bcbc4b857
Anders <ansol@dtu.dk>
2023-01-27 09:57:13 +0100

    2 rep

--- notebooks/Synthetic_fit.py
+++ notebooks/Synthetic_fit.py
@@ -55,7 +55,7 @@ data = torch.unsqueeze(torch.transpose(data,dim0=0,dim1=1),dim=0).float()
 
 # In[7]:
 
-num_reps = 5
+num_reps = 2
 best_LR = 0.01
 int_epoch = 1000
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
