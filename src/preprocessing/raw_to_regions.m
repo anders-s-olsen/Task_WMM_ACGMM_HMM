d99a8ff889c207ff3dd8432ab876f00f421c59e3
ansol <ansol@dtu.dk>
2023-02-21 12:10:23 +0100

    100 region data

--- src/preprocessing/raw_to_regions.m
+++ src/preprocessing/raw_to_regions.m
@@ -1,13 +1,16 @@
 clear
 
-datadir = dir('data/raw/**/*.nii.gz')
+datadir = dir('data/raw/**/*.nii.gz');
+for i = 1:numel(datadir)
+    gunzip([datadir(i).folder,'/',datadir(i).name]);
+end
 
-atlas = int16(niftiread('data/external/Schaefer2018_200Parcels_7Networks_order_FSLMNI152_3mm.nii'));
+atlas = int16(niftiread('data/external/Schaefer2018_100Parcels_7Networks_order_FSLMNI152_3mm.nii'));
 atlas = reshape(atlas,prod(size(atlas,[1,2,3])),1);
 
 
 for i = 1:numel(datadir)
-    V = niftiread([datadir(i).folder,'/',datadir(i).name]);
+    V = niftiread([datadir(i).folder,'/',datadir(i).name(1:end-3)]);
     V = reshape(V,prod(size(V,[1,2,3])),size(V,4));
     
     for region = 1:max(atlas)
@@ -16,10 +19,14 @@ for i = 1:numel(datadir)
         
     end
     
-    delete(['data/processed/atlas_',datadir(i).name(1:4),'.h5'])
-    h5create(['data/processed/atlas_',datadir(i).name(1:4),'.h5'],'/data',size(V_ROI))
-    h5write(['data/processed/atlas_',datadir(i).name(1:4),'.h5'],'/data',V_ROI)
+    delete(['data/processed/atlas100_',datadir(i).name(1:4),'.h5'])
+    h5create(['data/processed/atlas100_',datadir(i).name(1:4),'.h5'],'/data',size(V_ROI))
+    h5write(['data/processed/atlas100_',datadir(i).name(1:4),'.h5'],'/data',V_ROI)
     
     
     disp(['Done with ',num2str(i)])
+end
+
+for i = 1:numel(datadir)
+    delete([datadir(i).folder,'/',datadir(i).name(1:end-3)]);
 end
--- src/visualization/make_labels.m
+++ src/visualization/make_labels.m
@@ -1,9 +1,9 @@
-file = readtable('data/external/Schaefer200_yeo7_lut.txt');;
+file = readtable('data/external/Schaefer100_yeo7_lut.txt');
 
 labcell = file.Var5;
 
-for i = 1:200
+for i = 1:numel(labcell)
     labels{i} = labcell{i}(11:end);
 end
-save('data/external/labels_cell','labels')
+save('data/external/labels_cell_100','labels')
 % writetable(array2table(lab2'),'data/external/labels.txt','WriteVariableNames',false)
