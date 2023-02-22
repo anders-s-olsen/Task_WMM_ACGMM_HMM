c58b0e2066daf228c895bb058a31463cd0a126c2
Anders <ansol@dtu.dk>
2023-02-22 14:01:08 +0100

    fix

--- submitfiles/realkr_experiments.sh
+++ submitfiles/realkr_experiments.sh
@@ -1,40 +1,40 @@
 #!/bin/sh
-sed '$d' realk_template.sh
-echo 'python3 realdata_K_LS_splithalf.py 1' >> realk_template.sh
-bsub < realk_template.sh
+sed '$d' realkr_template.sh
+echo 'python3 realdata_K_LS_splithalf.py 1' >> realkr_template.sh
+bsub < realkr_template.sh
 
-sed '$d' realk_template.sh
-echo 'python3 realdata_K_LS_splithalf.py 2' >> realk_template.sh
-bsub < realk_template.sh
+sed '$d' realkr_template.sh
+echo 'python3 realdata_K_LS_splithalf.py 2' >> realkr_template.sh
+bsub < realkr_template.sh
 
-sed '$d' realk_template.sh
-echo 'python3 realdata_K_LS_splithalf.py 3' >> realk_template.sh
-bsub < realk_template.sh
+sed '$d' realkr_template.sh
+echo 'python3 realdata_K_LS_splithalf.py 3' >> realkr_template.sh
+bsub < realkr_template.sh
 
-sed '$d' realk_template.sh
-echo 'python3 realdata_K_LS_splithalf.py 4' >> realk_template.sh
-bsub < realk_template.sh
+sed '$d' realkr_template.sh
+echo 'python3 realdata_K_LS_splithalf.py 4' >> realkr_template.sh
+bsub < realkr_template.sh
 
-sed '$d' realk_template.sh
-echo 'python3 realdata_K_LS_splithalf.py 5' >> realk_template.sh
-bsub < realk_template.sh
+sed '$d' realkr_template.sh
+echo 'python3 realdata_K_LS_splithalf.py 5' >> realkr_template.sh
+bsub < realkr_template.sh
 
-sed '$d' realk_template.sh
-echo 'python3 realdata_K_LS_splithalf.py 6' >> realk_template.sh
-bsub < realk_template.sh
+sed '$d' realkr_template.sh
+echo 'python3 realdata_K_LS_splithalf.py 6' >> realkr_template.sh
+bsub < realkr_template.sh
 
-sed '$d' realk_template.sh
-echo 'python3 realdata_K_LS_splithalf.py 7' >> realk_template.sh
-bsub < realk_template.sh
+sed '$d' realkr_template.sh
+echo 'python3 realdata_K_LS_splithalf.py 7' >> realkr_template.sh
+bsub < realkr_template.sh
 
-sed '$d' realk_template.sh
-echo 'python3 realdata_K_LS_splithalf.py 8' >> realk_template.sh
-bsub < realk_template.sh
+sed '$d' realkr_template.sh
+echo 'python3 realdata_K_LS_splithalf.py 8' >> realkr_template.sh
+bsub < realkr_template.sh
 
-sed '$d' realk_template.sh
-echo 'python3 realdata_K_LS_splithalf.py 9' >> realk_template.sh
-bsub < realk_template.sh
+sed '$d' realkr_template.sh
+echo 'python3 realdata_K_LS_splithalf.py 9' >> realkr_template.sh
+bsub < realkr_template.sh
 
-sed '$d' realk_template.sh
-echo 'python3 realdata_K_LS_splithalf.py 10' >> realk_template.sh
-bsub < realk_template.sh
+sed '$d' realkr_template.sh
+echo 'python3 realdata_K_LS_splithalf.py 10' >> realkr_template.sh
+bsub < realkr_template.sh
