362ecdd2dac9ba072c240974911b35a2b99adb41
Anders <ansol@dtu.dk>
2023-02-25 12:43:32 +0100

    1node

--- notebooks/realdata_k.py
+++ notebooks/realdata_k.py
@@ -173,5 +173,5 @@ def run_experiment(K):
                             np.savetxt('../data/real_K/K'+str(K)+'Watson_HMM_comp'+str(kk)+'_mu'+str(r)+'.csv',param['emission_model_'+str(kk)]['mu'].detach())
                             np.savetxt('../data/real_K/K'+str(K)+'Watson_HMM_comp'+str(kk)+'_kappa'+str(r)+'.csv',param['emission_model_'+str(kk)]['kappa'].detach())
 if __name__=="__main__":
-    run_experiment(K=int(sys.argv[1]))
-    #run_experiment(K=4)
+    #run_experiment(K=int(sys.argv[1]))
+    run_experiment(K=4)
