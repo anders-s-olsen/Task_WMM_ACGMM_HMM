a1a6cc52b9b2307ef0446f12e486dea3286a82d6
Anders <ansol@dtu.dk>
2023-02-25 18:33:31 +0100

    fix

--- submitfiles/real_K_experiments.sh
+++ submitfiles/real_K_experiments.sh
@@ -3,7 +3,7 @@ for K in 1 2 3 4 5 6 7 8 9 10
 do
 
     sed -i '$ d' real_K_template.sh
-    echo "python3 realdata_fit.py $K" >> real_K_template.sh
+    echo "python3 realdata_k.py $K" >> real_K_template.sh
     bsub < real_K_template.sh
 
 done
