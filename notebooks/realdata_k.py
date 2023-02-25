f5b20214250d870aa803bc1c2e9eb05ff70ffa2f
Anders <ansol@dtu.dk>
2023-02-25 12:33:25 +0100

    speed test

--- notebooks/realdata_k.py
+++ notebooks/realdata_k.py
@@ -23,7 +23,7 @@ from src.models.AngularCentralGauss_torch import AngularCentralGaussian as ACG
 from src.models.Watson_torch import Watson
 from src.models.MixtureModel_torch import TorchMixtureModel
 from src.models.HMM_torch import HiddenMarkovModel as HMM
-from src.various.training import train_hmm
+from src.various.training import train_hmm_batch, train_hmm,train_hmm_lbfgs,train_hmm_subject
 
 torch.set_num_threads(16)
 
@@ -38,10 +38,11 @@ def get_param(model, show=True):
     
     return para
 
+
 def run_experiment(K):
     num_repsouter = 5
     num_repsinner = 1
-    int_epoch = 1000000
+    int_epoch = 10000
     num_regions = 100
 
     datah5 = h5py.File('../data/processed/dataset_all_subjects_LEiDA_100.hdf5', 'r')
@@ -54,6 +55,7 @@ def run_experiment(K):
             
     data_train_concat = torch.concatenate([data_train[sub] for sub in range(data_train.shape[0])])
     data_test_concat = torch.concatenate([data_test[sub] for sub in range(data_test.shape[0])])
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     #for m in range(4):
     for r in range(num_repsouter):
         for m in range(4):
@@ -61,24 +63,29 @@ def run_experiment(K):
             for r2 in range(num_repsinner):
                 if m==0:
                     model0 = TorchMixtureModel(distribution_object=ACG,K=K, dist_dim=data_train.shape[2],regu=1e-2)
-                    optimizer = optim.Adam(model0.parameters(), lr=0.01)
-                    like,model0,like_best = train_hmm(model0, data=data_train_concat, optimizer=optimizer, num_epoch=int_epoch, keep_bar=False,early_stopping=True)
-                    test_like = -model0.log_likelihood_mixture(data_test_concat)
-
+                    optimizer = optim.Adam(model0.parameters(), lr=1)
+                    like,model0,like_best = train_hmm_batch(model0, data=data_train, optimizer=optimizer, num_epoch=int_epoch, keep_bar=False,early_stopping=True,modeltype=0)
+                    #like,model0,like_best = train_hmm(model0, data=data_train_concat, optimizer=optimizer, num_epoch=int_epoch, keep_bar=False,early_stopping=True)
+                    #like = train_hmm_lbfgs(model0, data=data_train_concat, optimizer=optimizer, num_epoch=int_epoch, keep_bar=False,early_stopping=False)
+                    #like,model0,like_best = train_hmm_subject(model0, data=data_train, optimizer=optimizer, num_epoch=int_epoch, keep_bar=False,early_stopping=True,modeltype=0)
+                    test_like = -model0.log_likelihood_mixture(data_test_concat.to(device))
+                    return
+                    plt.figure(),plt.plot(like)
                     param = get_param(model0)
                     A = torch.zeros((K,data_train.shape[2],data_train.shape[2]))
                     for kk in range(K):
                         A[kk] = torch.linalg.pinv(param['mix_comp_'+str(kk)]@param['mix_comp_'+str(kk)].T)
+                        plt.figure(),plt.imshow(A[kk]),plt.colorbar()
                         np.savetxt('../data/real_K/K'+str(K)+'ACG_MM_comp'+str(kk)+'_'+str(r)+'.csv',A[kk].detach())
                     np.savetxt('../data/real_K/K'+str(K)+'ACG_MM_testlikelihood'+str(r)+'.csv',np.array((test_like.detach(),K)))
                     np.savetxt('../data/real_K/K'+str(K)+'ACG_MM_likelihood'+str(r)+'.csv',like)
-                    post = model0.posterior(data_test_concat)
+                    post = model0.posterior(data_test_concat.to(device))
                     np.savetxt('../data/real_K/K'+str(K)+'ACG_MM_assignment'+str(r)+'.csv',np.transpose(post.detach()))
                 elif m==1:
                     model1 = HMM(num_states=K, observation_dim=data_train.shape[2], emission_dist=ACG,regu=1e-2)
                     optimizer = optim.Adam(model1.parameters(), lr=0.1)
-                    like,model1,like_best = train_hmm(model1, data=data_train, optimizer=optimizer, num_epoch=int_epoch, keep_bar=False,early_stopping=True)
-                    test_like = -model1.forward(data_test)
+                    like,model1,like_best = train_hmm_batch(model1, data=data_train, optimizer=optimizer, num_epoch=int_epoch, keep_bar=False,early_stopping=True,modeltype=1)
+                    test_like = -model1.forward(data_test.to(device))
                     param = get_param(model1)
                     A = torch.zeros((K,data_train.shape[2],data_train.shape[2]))
                     for kk in range(K):
@@ -86,33 +93,33 @@ def run_experiment(K):
                         np.savetxt('../data/real_K/K'+str(K)+'ACG_HMM_comp'+str(kk)+'_'+str(r)+'.csv',A[kk].detach())
                     np.savetxt('../data/real_K/K'+str(K)+'ACG_HMM_testlikelihood'+str(r)+'.csv',np.array((test_like.detach(),K)))
                     np.savetxt('../data/real_K/K'+str(K)+'ACG_HMM_likelihood'+str(r)+'.csv',like)
-                    post = model1.viterbi2(data_test)
+                    post = model1.viterbi2(data_test.to(device))
                     np.savetxt('../data/real_K/K'+str(K)+'ACG_HMM_assignment'+str(r)+'.csv',np.transpose(post.detach()))
                 elif m==2:
                     model2 = TorchMixtureModel(distribution_object=Watson,K=K, dist_dim=data_train.shape[2])
                     optimizer = optim.Adam(model2.parameters(), lr=0.01)
-                    like,model2,like_best = train_hmm(model2, data=data_train_concat, optimizer=optimizer, num_epoch=int_epoch, keep_bar=False,early_stopping=True)
-                    test_like = -model2.log_likelihood_mixture(data_test_concat)
+                    like,model2,like_best = train_hmm_batch(model2, data=data_train_concat, optimizer=optimizer, num_epoch=int_epoch, keep_bar=False,early_stopping=True,modeltype=0)
+                    test_like = -model2.log_likelihood_mixture(data_test_concat.to(device))
                     np.savetxt('../data/real_K/K'+str(K)+'Watson_MM_testlikelihood'+str(r)+'.csv',np.array((test_like.detach(),K)))
                     np.savetxt('../data/real_K/K'+str(K)+'Watson_MM_likelihood'+str(r)+'.csv',like)
                     param = get_param(model2)
                     for kk in range(K):
                         np.savetxt('../data/real_K/K'+str(K)+'Watson_MM_comp'+str(kk)+'_mu'+str(r)+'.csv',param['mix_comp_'+str(kk)]['mu'].detach())
                         np.savetxt('../data/real_K/K'+str(K)+'Watson_MM_comp'+str(kk)+'_kappa'+str(r)+'.csv',param['mix_comp_'+str(kk)]['kappa'].detach())
-                    post = model2.posterior(data_test_concat)
+                    post = model2.posterior(data_test_concat.to(device))
                     np.savetxt('../data/real_K/K'+str(K)+'Watson_MM_assignment'+str(r)+'.csv',np.transpose(post.detach()))
                 elif m==3:
                     model3 = HMM(num_states=K, observation_dim=data_train.shape[2], emission_dist=Watson)
                     optimizer = optim.Adam(model3.parameters(), lr=0.01)
-                    like,model3,like_best = train_hmm(model3, data=data_train, optimizer=optimizer, num_epoch=int_epoch, keep_bar=False,early_stopping=True)
-                    test_like = -model3.forward(data_test)
+                    like,model3,like_best = train_hmm_batch(model3, data=data_train, optimizer=optimizer, num_epoch=int_epoch, keep_bar=False,early_stopping=True,modeltype=1)
+                    test_like = -model3.forward(data_test.to(device))
                     np.savetxt('../data/real_K/K'+str(K)+'Watson_HMM_testlikelihood'+str(r)+'.csv',np.array((test_like.detach(),K)))
                     np.savetxt('../data/real_K/K'+str(K)+'Watson_HMM_likelihood'+str(r)+'.csv',like)
                     param = get_param(model3)
                     for kk in range(K):
                         np.savetxt('../data/real_K/K'+str(K)+'Watson_HMM_comp'+str(kk)+'_mu'+str(r)+'.csv',param['emission_model_'+str(kk)]['mu'].detach())
                         np.savetxt('../data/real_K/K'+str(K)+'Watson_HMM_comp'+str(kk)+'_kappa'+str(r)+'.csv',param['emission_model_'+str(kk)]['kappa'].detach())
-                    post = model3.viterbi2(data_test)
+                    post = model3.viterbi2(data_test.to(device))
                     np.savetxt('../data/real_K/K'+str(K)+'Watson_HMM_assignment'+str(r)+'.csv',np.transpose(post.detach()))
                 
                 continue
@@ -167,4 +174,4 @@ def run_experiment(K):
                             np.savetxt('../data/real_K/K'+str(K)+'Watson_HMM_comp'+str(kk)+'_kappa'+str(r)+'.csv',param['emission_model_'+str(kk)]['kappa'].detach())
 if __name__=="__main__":
     run_experiment(K=int(sys.argv[1]))
-    #run_experiment(K=1)
+    #run_experiment(K=4)
--- src/various/training.py
+++ src/various/training.py
@@ -3,6 +3,92 @@ import numpy as np
 
 from tqdm import tqdm
 
+##### Batch training
+
+def train_hmm_batch(HMM, data, optimizer, num_epoch=100, keep_bar=True,early_stopping=False,modeltype=0):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print('Currently using device: ')
+    print(device)
+    model = HMM.to(device).train()
+
+    epoch_likelihood_collector = np.zeros(num_epoch)
+
+    subject_leida_vectors = data.to(device)
+
+    for epoch in tqdm(range(num_epoch), leave=keep_bar):
+
+        indices = torch.randperm(data.shape[0])
+        epoch_likelihood = 0
+        for batch in range(6):
+            if batch<5:
+                idx = indices[:5]
+                indices = indices[5:]
+            else:
+                idx = indices
+            if modeltype==0:
+                batch_likelihood = -model(torch.concatenate([subject_leida_vectors[idx][sub] for sub in range(len(idx))]))  # OBS! Negative
+            else:
+                batch_likelihood = -model(subject_leida_vectors[idx])  # OBS! Negative
+            epoch_likelihood += batch_likelihood
+            optimizer.zero_grad(set_to_none=True)
+            batch_likelihood.backward()
+            optimizer.step()
+
+        epoch_likelihood_collector[epoch] = epoch_likelihood
+        if early_stopping:
+            if epoch==0:
+                ident = torch.randint(0,10000000,(1,1)).detach()
+                torch.save(model.state_dict(),'../data/interim/model_checkpoint'+str(ident)+'.pt')
+                best_like = epoch_likelihood_collector[epoch]
+            elif np.isin(epoch,np.linspace(0,num_epoch,int(num_epoch/100+1))):
+                if epoch_likelihood_collector[epoch]<epoch_likelihood_collector[epoch-5] and epoch_likelihood_collector[epoch]<best_like:
+                    torch.save(model.state_dict(),'../data/interim/model_checkpoint'+str(ident)+'.pt')
+                    np.savetxt('../data/interim/likelihood'+str(ident)+'.txt',np.array((epoch,epoch_likelihood_collector[epoch])))
+                    best_like = epoch_likelihood_collector[epoch]
+                    like_best = np.array((epoch,epoch_likelihood_collector[epoch]))
+
+def train_hmm_subject(HMM, data, optimizer, num_epoch=100, keep_bar=True,early_stopping=False,modeltype=0):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print('Currently using device: ')
+    print(device)
+    model = HMM.to(device).train()
+
+    epoch_likelihood_collector = np.zeros(num_epoch)
+
+    subject_leida_vectors = data.to(device)
+
+    for epoch in tqdm(range(num_epoch), leave=keep_bar):
+
+        epoch_likelihood = 0
+        for subject in data:
+            if modeltype==0:
+                subject_likelihood = -model(subject)  # OBS! Negative
+            else:
+                subject_likelihood = -model(subject[None,:,:])  # OBS! Negative
+            epoch_likelihood += subject_likelihood
+            optimizer.zero_grad(set_to_none=True)
+            subject_likelihood.backward()
+            optimizer.step()
+
+        epoch_likelihood_collector[epoch] = epoch_likelihood
+        if early_stopping:
+            if epoch==0:
+                ident = torch.randint(0,10000000,(1,1)).detach()
+                torch.save(model.state_dict(),'../data/interim/model_checkpoint'+str(ident)+'.pt')
+                best_like = epoch_likelihood_collector[epoch]
+            elif np.isin(epoch,np.linspace(0,num_epoch,int(num_epoch/100+1))):
+                if epoch_likelihood_collector[epoch]<epoch_likelihood_collector[epoch-5] and epoch_likelihood_collector[epoch]<best_like:
+                    torch.save(model.state_dict(),'../data/interim/model_checkpoint'+str(ident)+'.pt')
+                    np.savetxt('../data/interim/likelihood'+str(ident)+'.txt',np.array((epoch,epoch_likelihood_collector[epoch])))
+                    best_like = epoch_likelihood_collector[epoch]
+                    like_best = np.array((epoch,epoch_likelihood_collector[epoch]))
+    if early_stopping:
+        model.load_state_dict(torch.load('../data/interim/model_checkpoint'+str(ident)+'.pt'))
+        return epoch_likelihood_collector,model,like_best
+    else: 
+        return epoch_likelihood_collector
+
+
 # Train Mixture model
 def train_mixture(MixtureModel, data, optimizer, num_epoch=100, keep_bar=True):
     device = 'cpu'
@@ -127,6 +213,46 @@ def train_hmm_subjects(HMM, data, optimizer, num_epoch=100, print_progress=False
     return epoch_likelihood_collector
 
 
+
+def train_hmm_lbfgs(HMM, data, optimizer, num_epoch=100, keep_bar=True,early_stopping=False):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print('Currently using device: ')
+    print(device)
+    model = HMM.to(device).train()
+
+    lbfgs = torch.optim.LBFGS(model.parameters())
+
+    epoch_likelihood_collector = np.zeros(num_epoch)
+    subject_leida_vectors = data.to(device)
+    def closure():
+            lbfgs.zero_grad()
+            objective = -model(subject_leida_vectors)
+            objective.backward()
+            return objective
+    for epoch in tqdm(range(num_epoch), leave=keep_bar):
+
+        NegativeLogLikelihood = -model(subject_leida_vectors)  # OBS! Negative
+        lbfgs.step(closure)
+
+        epoch_likelihood_collector[epoch] = NegativeLogLikelihood
+        if early_stopping:
+            if epoch==0:
+                ident = torch.randint(0,10000000,(1,1)).detach()
+                torch.save(model.state_dict(),'../data/interim/model_checkpoint'+str(ident)+'.pt')
+                best_like = epoch_likelihood_collector[epoch]
+            elif np.isin(epoch,np.linspace(0,num_epoch,int(num_epoch/100+1))):
+                if epoch_likelihood_collector[epoch]<epoch_likelihood_collector[epoch-5] and epoch_likelihood_collector[epoch]<best_like:
+                    torch.save(model.state_dict(),'../data/interim/model_checkpoint'+str(ident)+'.pt')
+                    np.savetxt('../data/interim/likelihood'+str(ident)+'.txt',np.array((epoch,epoch_likelihood_collector[epoch])))
+                    best_like = epoch_likelihood_collector[epoch]
+                    like_best = np.array((epoch,epoch_likelihood_collector[epoch]))
+
+
+    if early_stopping:
+        model.load_state_dict(torch.load('../data/interim/model_checkpoint'+str(ident)+'.pt'))
+        return epoch_likelihood_collector,model,like_best
+    else: 
+        return epoch_likelihood_collector
 
 
 
