4c7a1f7338b69cfa09607f7571bccf87535e7281
Anders <ansol@dtu.dk>
2023-02-27 10:55:46 +0100

    rename

--- submitfiles/real_K_Watsontemplate.sh
+++ submitfiles/real_K_Watsontemplate.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-#BSUB -J real_ACG_K_job
+#BSUB -J real_Watson_K_job
 #BSUB -q hpc
 #BSUB -R "rusage[mem=4GB]"
 #BSUB -B
 #BSUB -N
-#BSUB -o real_ACG_K_job_out_%J.txt
-#BSUB -e real_ACG_K_job_err_%J.txt
+#BSUB -o real_Watson_K_job_out_%J.txt
+#BSUB -e real_Watson_K_job_err_%J.txt
 #BSUB -W 48:00 
 #BSUB -n 8
 #BSUB -R "span[hosts=1]"
@@ -18,4 +18,4 @@ module load h5py/3.7.0-python-3.10.7
 python3 -m pip install --user tqdm torch
 cd ..
 cd notebooks
-python3 realdata_K_chosenD.py 10
+python3 realdata_k.py 10
--- submitfiles/real_K_experiments.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-for K in 1 2 3 4 5 6 7 8 9 10
-do
-
-    sed -i '$ d' real_K_template.sh
-    echo "python3 realdata_k.py $K" >> real_K_template.sh
-    bsub < real_K_template.sh
-
-done
