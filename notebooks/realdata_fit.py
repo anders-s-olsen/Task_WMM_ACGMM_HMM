da795827644676e84a40d20bf0e6b08a2b657a4c
Anders <ansol@dtu.dk>
2023-02-28 09:51:43 +0100

    real fit

--- notebooks/realdata_fit.py
+++ notebooks/realdata_fit.py
@@ -20,6 +20,7 @@ import matplotlib.pyplot as plt
 sys.path.append(os.path.abspath(os.path.join('..')))
 
 from src.models.AngularCentralGauss_torch import AngularCentralGaussian as ACG
+from src.models.AngularCentralGauss_lowrank import AngularCentralGaussian as ACG_lowrank
 from src.models.Watson_torch import Watson
 from src.models.MixtureModel_torch import TorchMixtureModel
 from src.models.HMM_torch import HiddenMarkovModel as HMM
@@ -27,7 +28,7 @@ from src.various.training import train_hmm,train_hmm_batch
 
 torch.set_num_threads(16)
 
-def get_param(model, show=True):
+def get_param(model, show=False):
     para = model.get_model_param()
     
     if show:
@@ -38,88 +39,111 @@ def get_param(model, show=True):
     
     return para
 
-def run_experiment(K):
-    num_repsouter = 5
+def run_experiment(m,r):
+    num_repsouter = 1
     num_repsinner = 1
-    int_epoch = 25000
     num_regions = 100
+    K=4
 
     datah5 = h5py.File('../data/processed/dataset_all_subjects_LEiDA_100.hdf5', 'r')
     #print(len(datah5.keys()))
-    data_train = torch.zeros((29,240,num_regions))
+    data_train = torch.zeros((29,240,num_regions),dtype=torch.double)
+    data_train_W = torch.zeros((29,240,num_regions))
     for idx,subject in enumerate(list(datah5.keys())):
-        data_train[idx] = torch.tensor(datah5[subject])
+        data_train[idx] = torch.DoubleTensor(torch.tensor(datah5[subject]))
+        data_train_W[idx] = torch.tensor(datah5[subject])
             
     data_train_concat = torch.concatenate([data_train[sub] for sub in range(data_train.shape[0])])
+    data_train_concat_W = torch.concatenate([data_train_W[sub] for sub in range(data_train_W.shape[0])])
     #for m in range(4):
-    for m in range(4):
-        for r in range(num_repsouter):
-            thres_like = 1000000000000000
-            for r2 in range(num_repsinner):
-                if m==0:
-                    
-                    model0 = TorchMixtureModel(distribution_object=ACG,K=K, dist_dim=num_regions,regu=1e-5)
-                    optimizer = optim.Adam(model0.parameters(), lr=0.1)
-                    #like,model0,like_best = train_hmm(model0, data=data_train_concat, optimizer=optimizer, num_epoch=int_epoch, keep_bar=False,early_stopping=True)
-                    like = train_hmm_batch(model0, data=data_train, optimizer=optimizer, num_epoch=int_epoch, keep_bar=False,early_stopping=False)
-                    model = model0
-                elif m==1:
-                    model1 = HMM(num_states=K, observation_dim=num_regions, emission_dist=ACG,regu=1e-5)
-                    optimizer = optim.Adam(model1.parameters(), lr=0.1)
-                    like,model1,like_best = train_hmm(model1, data=data_train, optimizer=optimizer, num_epoch=int_epoch, keep_bar=False,early_stopping=False)
-                    model = model1
-                elif m==2:
-                    model2 = TorchMixtureModel(distribution_object=Watson,K=K, dist_dim=num_regions)
-                    optimizer = optim.Adam(model2.parameters(), lr=0.01)
-                    like,model2,like_best = train_hmm(model2, data=data_train_concat, optimizer=optimizer, num_epoch=int_epoch, keep_bar=False,early_stopping=False)
-                    model = model2
-                elif m==3:
-                    model3 = HMM(num_states=K, observation_dim=num_regions, emission_dist=Watson)
-                    optimizer = optim.Adam(model3.parameters(), lr=0.01)
-                    like,model3,like_best = train_hmm(model3, data=data_train, optimizer=optimizer, num_epoch=int_epoch, keep_bar=False,early_stopping=False)
-                    model = model3
+    if m==0:
+        for D in np.arange(15):
+            print(D)
+            if D==0:
+                model0 = TorchMixtureModel(distribution_object=ACG_lowrank,K=K, D=D+1,dist_dim=data_train.shape[2])
+            else:
+                model0 = TorchMixtureModel(distribution_object=ACG_lowrank,K=K, D=D+1,dist_dim=data_train.shape[2],init=init)
+            optimizer = optim.Adam(model0.parameters(), lr=0.1)
+            like = train_hmm_batch(model0, data=data_train, optimizer=optimizer, num_epoch=1000, keep_bar=False,early_stopping=False,modeltype=0)
+            param = get_param(model0)
+            init = {}
+            init['pi'] = param['un_norm_pi']
+            init['comp'] = torch.zeros((K,data_train.shape[2],D+1),dtype=torch.double)
+            for kk in range(K):
+                init['comp'][kk] = param['mix_comp_'+str(kk)]
+        
+    elif m==1:
+        
+        for D in np.arange(15):
+            print(D)
+            if D==0:
+                model1 = HMM(emission_dist=ACG_lowrank,K=K, D=D+1,observation_dim=data_train.shape[2])
+            else:
+                model1 = HMM(emission_dist=ACG_lowrank,K=K, D=D+1,observation_dim=data_train.shape[2],init=init)
+            optimizer = optim.Adam(model1.parameters(), lr=0.1)
+            like = train_hmm_batch(model1, data=data_train, optimizer=optimizer, num_epoch=1000, keep_bar=False,early_stopping=False,modeltype=1)
+            param = get_param(model1)
+            init = {}
+            init['pi'] = param['un_norm_priors']
+            init['T'] = param['un_norm_Transition_matrix']
+            init['comp'] = torch.zeros((K,data_train.shape[2],D+1),dtype=torch.double)
+            for kk in range(K):
+                init['comp'][kk] = param['emission_model_'+str(kk)]
 
-                param = get_param(model)
-                #plt.figure(),plt.plot(like)
-                #R = torch.zeros((K,num_regions,num_regions))
-                #A = torch.zeros((K,num_regions,num_regions))
-                #for kk in range(K):
-                #    R[kk] = param['mix_comp_'+str(kk)]@param['mix_comp_'+str(kk)].T
-                #    A[kk] = torch.linalg.pinv(R[kk])
-                #for kk in range(K):
-                #    plt.figure(),plt.imshow(A[kk]),plt.colorbar()
-                if m==0:
-                    post = model.posterior(data_train_concat)
-                    np.savetxt('../data/real_fit/K'+str(K)+'ACG_MM_likelihood'+str(r)+'.csv',like)
-                    np.savetxt('../data/real_fit/K'+str(K)+'ACG_MM_assignment'+str(r)+'.csv',np.transpose(post.detach()))
-                    np.savetxt('../data/real_fit/K'+str(K)+'ACG_MM_prior'+str(r)+'.csv',torch.nn.functional.softmax(param['un_norm_pi'],dim=0).detach())
-                    for kk in range(K):
-                        np.savetxt('../data/real_fit/K'+str(K)+'ACG_MM_comp'+str(kk)+'_'+str(r)+'.csv',torch.linalg.pinv(param['mix_comp_'+str(kk)]@param['mix_comp_'+str(kk)].T).detach())
-                elif m==1:
-                    best_path,xx,xxx = model.viterbi2(data_train)
-                    np.savetxt('../data/real_fit/K'+str(K)+'ACG_HMM_likelihood'+str(r)+'.csv',like)
-                    np.savetxt('../data/real_fit/K'+str(K)+'ACG_HMM_assignment'+str(r)+'.csv',np.transpose(best_path))
-                    np.savetxt('../data/real_fit/K'+str(K)+'ACG_HMM_prior'+str(r)+'.csv',torch.nn.functional.softmax(param['un_norm_priors'],dim=0).detach())
-                    np.savetxt('../data/real_fit/K'+str(K)+'ACG_HMM_T'+str(r)+'.csv',torch.nn.functional.softmax(param['un_norm_Transition_matrix'],dim=1).detach())
-                    for kk in range(K):
-                        np.savetxt('../data/real_fit/K'+str(K)+'ACG_HMM_comp'+str(kk)+'_'+str(r)+'.csv',torch.linalg.pinv(param['emission_model_'+str(kk)]@param['emission_model_'+str(kk)].T).detach())
-                elif m==2:
-                    post = model.posterior(data_train_concat)
-                    np.savetxt('../data/real_fit/K'+str(K)+'Watson_MM_likelihood'+str(r)+'.csv',like)
-                    np.savetxt('../data/real_fit/K'+str(K)+'Watson_MM_assignment'+str(r)+'.csv',np.transpose(post.detach()))
-                    np.savetxt('../data/real_fit/K'+str(K)+'Watson_MM_prior'+str(r)+'.csv',torch.nn.functional.softmax(param['un_norm_pi'],dim=0).detach())
-                    for kk in range(K):
-                        np.savetxt('../data/real_fit/K'+str(K)+'Watson_MM_comp'+str(kk)+'_mu'+str(r)+'.csv',param['mix_comp_'+str(kk)]['mu'].detach())
-                        np.savetxt('../data/real_fit/K'+str(K)+'Watson_MM_comp'+str(kk)+'_kappa'+str(r)+'.csv',param['mix_comp_'+str(kk)]['kappa'].detach())
-                elif m==3:
-                    best_path,xx,xxx = model.viterbi2(data_train)
-                    np.savetxt('../data/real_fit/K'+str(K)+'Watson_HMM_likelihood'+str(r)+'.csv',like)
-                    np.savetxt('../data/real_fit/K'+str(K)+'Watson_HMM_assignment'+str(r)+'.csv',np.transpose(best_path))
-                    np.savetxt('../data/real_fit/K'+str(K)+'Watson_HMM_prior'+str(r)+'.csv',torch.nn.functional.softmax(param['un_norm_priors'],dim=0).detach())
-                    np.savetxt('../data/real_fit/K'+str(K)+'Watson_HMM_T'+str(r)+'.csv',torch.nn.functional.softmax(param['un_norm_Transition_matrix'],dim=1).detach())
-                    for kk in range(K):
-                        np.savetxt('../data/real_fit/K'+str(K)+'Watson_HMM_comp'+str(kk)+'_mu'+str(r)+'.csv',param['emission_model_'+str(kk)]['mu'].detach())
-                        np.savetxt('../data/real_fit/K'+str(K)+'Watson_HMM_comp'+str(kk)+'_kappa'+str(r)+'.csv',param['emission_model_'+str(kk)]['kappa'].detach())
+    elif m==2:
+        model2 = TorchMixtureModel(distribution_object=Watson,K=K, dist_dim=num_regions)
+        optimizer = optim.Adam(model2.parameters(), lr=0.1)
+        like = train_hmm_batch(model2, data=data_train_W, optimizer=optimizer, num_epoch=25000, keep_bar=False,early_stopping=False,modeltype=0)
+    elif m==3:
+        model3 = HMM(num_states=K, observation_dim=num_regions, emission_dist=Watson)
+        optimizer = optim.Adam(model3.parameters(), lr=0.1)
+        like = train_hmm_batch(model3, data=data_train_W, optimizer=optimizer, num_epoch=25000, keep_bar=False,early_stopping=False,modeltype=1)
+
+    
+    #plt.figure(),plt.plot(like)
+    #R = torch.zeros((K,num_regions,num_regions))
+    #A = torch.zeros((K,num_regions,num_regions))
+    #for kk in range(K):
+    #    R[kk] = param['mix_comp_'+str(kk)]@param['mix_comp_'+str(kk)].T
+    #    A[kk] = torch.linalg.pinv(R[kk])
+    #for kk in range(K):
+    #    plt.figure(),plt.imshow(A[kk]),plt.colorbar()
+    if m==0:
+        param = get_param(model0)
+        post = model0.posterior(data_train_concat)
+        np.savetxt('../data/real_fit/K'+str(K)+'ACG_MM_likelihood'+str(r)+'.csv',like)
+        np.savetxt('../data/real_fit/K'+str(K)+'ACG_MM_assignment'+str(r)+'.csv',np.transpose(post.detach()))
+        np.savetxt('../data/real_fit/K'+str(K)+'ACG_MM_prior'+str(r)+'.csv',torch.nn.functional.softmax(param['un_norm_pi'],dim=0).detach())
+        for kk in range(K):
+            np.savetxt('../data/real_fit/K'+str(K)+'ACG_MM_comp'+str(kk)+'_'+str(r)+'.csv',torch.linalg.pinv(param['mix_comp_'+str(kk)]@param['mix_comp_'+str(kk)].T).detach())
+    elif m==1:
+        param = get_param(model1)
+        best_path,xx,xxx = model1.viterbi2(data_train)
+        np.savetxt('../data/real_fit/K'+str(K)+'ACG_HMM_likelihood'+str(r)+'.csv',like)
+        np.savetxt('../data/real_fit/K'+str(K)+'ACG_HMM_assignment'+str(r)+'.csv',np.transpose(best_path))
+        np.savetxt('../data/real_fit/K'+str(K)+'ACG_HMM_prior'+str(r)+'.csv',torch.nn.functional.softmax(param['un_norm_priors'],dim=0).detach())
+        np.savetxt('../data/real_fit/K'+str(K)+'ACG_HMM_T'+str(r)+'.csv',torch.nn.functional.softmax(param['un_norm_Transition_matrix'],dim=1).detach())
+        for kk in range(K):
+            np.savetxt('../data/real_fit/K'+str(K)+'ACG_HMM_comp'+str(kk)+'_'+str(r)+'.csv',torch.linalg.pinv(param['emission_model_'+str(kk)]@param['emission_model_'+str(kk)].T).detach())
+    elif m==2:
+        param = get_param(model2)
+        post = model2.posterior(data_train_concat_W)
+        np.savetxt('../data/real_fit/K'+str(K)+'Watson_MM_likelihood'+str(r)+'.csv',like)
+        np.savetxt('../data/real_fit/K'+str(K)+'Watson_MM_assignment'+str(r)+'.csv',np.transpose(post.detach()))
+        np.savetxt('../data/real_fit/K'+str(K)+'Watson_MM_prior'+str(r)+'.csv',torch.nn.functional.softmax(param['un_norm_pi'],dim=0).detach())
+        for kk in range(K):
+            np.savetxt('../data/real_fit/K'+str(K)+'Watson_MM_comp'+str(kk)+'_mu'+str(r)+'.csv',param['mix_comp_'+str(kk)]['mu'].detach())
+            np.savetxt('../data/real_fit/K'+str(K)+'Watson_MM_comp'+str(kk)+'_kappa'+str(r)+'.csv',param['mix_comp_'+str(kk)]['kappa'].detach())
+    elif m==3:
+        param = get_param(model3)
+        best_path,xx,xxx = model3.viterbi2(data_train_W)
+        np.savetxt('../data/real_fit/K'+str(K)+'Watson_HMM_likelihood'+str(r)+'.csv',like)
+        np.savetxt('../data/real_fit/K'+str(K)+'Watson_HMM_assignment'+str(r)+'.csv',np.transpose(best_path))
+        np.savetxt('../data/real_fit/K'+str(K)+'Watson_HMM_prior'+str(r)+'.csv',torch.nn.functional.softmax(param['un_norm_priors'],dim=0).detach())
+        np.savetxt('../data/real_fit/K'+str(K)+'Watson_HMM_T'+str(r)+'.csv',torch.nn.functional.softmax(param['un_norm_Transition_matrix'],dim=1).detach())
+        for kk in range(K):
+            np.savetxt('../data/real_fit/K'+str(K)+'Watson_HMM_comp'+str(kk)+'_mu'+str(r)+'.csv',param['emission_model_'+str(kk)]['mu'].detach())
+            np.savetxt('../data/real_fit/K'+str(K)+'Watson_HMM_comp'+str(kk)+'_kappa'+str(r)+'.csv',param['emission_model_'+str(kk)]['kappa'].detach())
 if __name__=="__main__":
-    run_experiment(K=int(sys.argv[1]))
-    #run_experiment(K=8)
+    run_experiment(m=int(sys.argv[1]),r=int(sys.argv[2]))
+    #run_experiment(K=4)
---
+++ submitfiles/real_fit_experiments.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+for m in 0 1 2 3
+do
+for r in 0 1 2 3 4
+do
+
+    sed -i '$ d' real_fit_template.sh
+    echo "python3 realdata_fit.py $m $r" >> real_fit_template.sh
+    bsub < real_fit_template.sh
+
+done
+done
